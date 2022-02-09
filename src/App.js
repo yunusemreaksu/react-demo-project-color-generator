2060b3fb1153f025fe7597699c7533efffd514b0
Yunus Emre Aksu <emre_aksu@live.com>
2022-02-09 14:57:16 +0300

    Added try and catch funtion

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10)
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   }
   return (
     <React.Fragment>
@@ -22,6 +27,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)} 
             placeholder="#f15025"
+            className={`${error ? "error" : null}`}
             />
             <button className='btn' type='submit'>submit</button>
         </form>
